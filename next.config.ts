6cf20fab7fbe5d99c7f9dff1f4c880a8de6d939c
Vicente Woinilowicz <139511901+woinivincent@users.noreply.github.com>
2024-11-23 17:16:30 -0300

    fix config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
+
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
+   
     ignoreDuringBuilds: true,
     
   },
