85205d763b7c6de2cbfbd1471b904a0785cc5152
Vicente Woinilowicz <139511901+woinivincent@users.noreply.github.com>
2025-07-14 01:53:06 -0300

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es" suppressHydrationWarning >
-       <link rel="icon" href="/assets/logoicon.png" />
+       <link rel="icon" href="/public/logoicon.png" />
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
